abdff3a14ea12c285d2335a426c1a90056ca38e9
Jokubas126 <jokubas126@users.noreply.github.com>
2019-12-13 22:02:10 +0100

    ListView added and random titles of notes added to them

--- app/src/main/java/com/example/mynotes/presenter/NoteListActivityPresenter.java
+++ app/src/main/java/com/example/mynotes/presenter/NoteListActivityPresenter.java
@@ -1,8 +1,11 @@
 package com.example.mynotes.presenter;
 
+import android.util.Log;
+
 import com.example.mynotes.model.data.Note;
 import com.example.mynotes.model.handlers.DBHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NoteListActivityPresenter {
@@ -17,6 +20,7 @@ public class NoteListActivityPresenter {
     public interface NoteListActivityView {
         void setNoteTitle(String title);
         void setNoteText(String text);
+        void addNoteTitleToListView(List<String> titleList);
     }
 
     public void addNote(String title, String text){
@@ -37,13 +41,15 @@ public class NoteListActivityPresenter {
         view.setNoteText(note.getText());
     }
 
-    public void loadNotes(){
-        handler.getNoteList();
+    public void loadAllNotes(){
+        List<String> noteTitleList = new ArrayList<>();
 
-        /*List<Note> noteList = handler.getNoteList();
+        List<Note> noteList = handler.getNoteList();
         for(Note note : noteList){
-            Log.d("Main Activity", "onCreate: " + note.getId() + " "
-                            + note.getTitle() + " " + note.getText());
-        }*/
+            Log.d("NoteListPresenter", "onLoadNotes " + note.getId() + " "  + note.getTitle() + " " + note.getText());
+            noteTitleList.add(note.getTitle());
+        }
+        //add to our listview
+        view.addNoteTitleToListView(noteTitleList);
     }
 }
--- app/src/main/java/com/example/mynotes/view/NoteListActivity.java
+++ app/src/main/java/com/example/mynotes/view/NoteListActivity.java
@@ -3,6 +3,11 @@ package com.example.mynotes.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.mynotes.R;
 import com.example.mynotes.model.handlers.DBHandler;
@@ -15,22 +20,50 @@ public class NoteListActivity extends AppCompatActivity
 
     NoteListActivityPresenter presenter;
 
+    private ListView listView;
+
+
+    TextView textTextView;
+    TextView titleTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.note_list_view_activity);
         presenter = new NoteListActivityPresenter(this, new DBHandler(NoteListActivity.this));
 
+        listView = findViewById(R.id.listview);
 
+        presenter.loadAllNotes();
     }
 
     @Override
     public void setNoteTitle(String title) {
-
+        titleTextView.setText(title);
     }
 
     @Override
     public void setNoteText(String text) {
+        textTextView.setText(text);
+    }
 
+    @Override
+    public void addNoteTitleToListView(List<String> titleList) {
+        //create array adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                titleList
+        );
+
+        //add to listview
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //do something when certain item is clicked
+            }
+        });
     }
 }
